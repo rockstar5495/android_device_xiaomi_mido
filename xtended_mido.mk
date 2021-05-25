e3b42d1d8a49b9ae25d5fa1e47db9fcea2dd66b6
rockstar5495 <pratyakshom.05@gmail.com>
2021-05-25 05:15:53 +0000

    mido: get Xtended
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- xtended_mido.mk
+++ xtended_mido.mk
@@ -21,12 +21,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+
+# Xtended Stuff
+XTENDED_BUILD_TYPE := UNOFFICIAL
+XTENDED_BUILD_MAINTAINER := Pratyaksh.Bharadwaj
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SCREEN_DENSITY := 440
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := xtended_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
