4e622ddafe24487a167cf491bd76649ff1a64ba1
Pratyaksh Bharadwaj <54440925+rockstar5495@users.noreply.github.com>
2021-06-01 22:00:19 +0530

    mido: adapt to TenX
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,14 @@
 rm -rf hardware/qcom-caf/msm8996/audio
 rm -rf hardware/qcom-caf/msm8996/display
 rm -rf hardware/qcom-caf/msm8996/media
+rm -rf hardware/qcom-caf/msm8996-r/audio
+rm -rf hardware/qcom-caf/msm8996-r/display
+rm -rf hardware/qcom-caf/msm8996-r/media
 
 # New HALS
-git clone https://github.com/rockstar5495/android_hardware_qcom_audio.git -b r31 hardware/qcom-caf/msm8996/audio
-git clone https://github.com/rockstar5495/android_hardware_qcom_display.git -b r31 hardware/qcom-caf/msm8996/display
-git clone https://github.com/rockstar5495/android_hardware_qcom_media.git -b r31 hardware/qcom-caf/msm8996/media
+git clone https://github.com/rockstar5495/android_hardware_qcom_audio.git -b r31 hardware/qcom-caf/msm8996-r/audio
+git clone https://github.com/rockstar5495/android_hardware_qcom_display.git -b r31 hardware/qcom-caf/msm8996-r/display
+git clone https://github.com/rockstar5495/android_hardware_qcom_media.git -b r31 hardware/qcom-caf/msm8996-r/media
 
 # Telephony
 rm -rf vendor/codeaurora/telephony
